25ec4805f2ec0a238a6be18126978479cae71c9c
Orlando Moraes Martins <orlandomoraesmartins@gmail.com>
2022-12-23 19:10:31 -0300

    Initial Commit

---
+++ SocialNetwork/SocialNetworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialNetworkApp.swift
+//  SocialNetwork
+//
+//  Created by Orlando Moraes Martins on 23/12/22.
+//
+
+import SwiftUI
+
+@main
+struct SocialNetworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
